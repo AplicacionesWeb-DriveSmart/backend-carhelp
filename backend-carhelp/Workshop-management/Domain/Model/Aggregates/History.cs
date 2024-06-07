e792c8d34188894c8e909f2d3bcb53c60dd53b4b
CarlosG345 <u202210364@upc.edu.pe>
2024-06-07 01:29:06 -0500

    feat: created aggregate history

--- backend-carhelp/Workshop-management/Domain/Model/Aggregates/History.cs
+++ backend-carhelp/Workshop-management/Domain/Model/Aggregates/History.cs
@@ -1,6 +1,24 @@
+using backend_carhelp.Workshop_management.Domain.Model.Commands;
+
 namespace backend_carhelp.Workshop_management.Domain.Model.Aggregates;
 
 public class History
 {
+    private int Id { get; }
+    private string Service_date { get; set; }
+    private string Description { get; set; }
+    private double Cost { get; set; }
+    private int Mileage { get; set; }
+    
+    public History(string serviceDate, string description, double cost, int mileage)
+    {
+        Service_date = serviceDate;
+        Description = description;
+        Cost = cost;
+        Mileage = mileage;
+    }
+
+    public History(CreateHistoryCommand command) 
+        : this(command.Service_date, command.Description, command.Cost, command.Mileage) {}
     
 }
