effed5a1324c4a3bd140921a30fc94311ddd5366
JuanMontes18 <u202210775@upc.edu.pe>
2024-06-25 23:34:19 -0500

    feat: added product controller

---
+++ backend-carhelp/Workshop-management/Interfaces/REST/ProductController.cs
@@ -0,0 +1,39 @@
+using System.Net.Mime;
+using backend_carhelp.Workshop_management.Domain.Model.Queries;
+using backend_carhelp.Workshop_management.Domain.Services;
+using backend_carhelp.Workshop_management.Interfaces.REST.Resources;
+using backend_carhelp.Workshop_management.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_carhelp.Workshop_management.Interfaces.REST;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class ProductController(IProductCommandService productCommandService, IProductQueryService productQueryService): ControllerBase
+{
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetProductById(int id)
+    {
+        var getProductByIdQuery = new GetProductByIdQuery(id);
+        var product = await productQueryService.Handle(getProductByIdQuery);
+        if(product == null) {
+            return NotFound();
+        }
+        var productResource = ProductResourceFromEntityAssembler.ToResourceFromEntity(product);
+        return Ok(productResource);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(CreateProductResource resource)
+    {
+        var createProductCommand = CreateProductCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var product = await productCommandService.Handle(createProductCommand);
+        if (product == null)
+        {
+            return BadRequest();
+        }
+        var productResource = ProductResourceFromEntityAssembler.ToResourceFromEntity(product);
+        return CreatedAtAction(nameof(GetProductById), new { id = productResource.Id }, productResource);
+    }
+}
